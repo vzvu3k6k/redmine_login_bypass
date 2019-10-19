d61dc76b13049497a216c1327383dedf48cb027e
vzvu3k6k <vzvu3k6k@gmail.com>
2019-10-20 04:49:00 +0900

    Disable other envs than development (#5)
    Note: Redmine 3.x and 3.x supports > Ruby 2.4, which doesn't support top-level return.

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ This is a Redmine plugin allowing developers to login without passwords.
 $ git clone https://github.com/vzvu3k6k/redmine_login_bypass.git redmine/plugins
 ```
 
+This plugin is disabled on non-`development` environment. Set `ENABLE_REDMINE_LOGIN_BYPASS=1` to enable.
+
 ## Supported Redmine versions
 
 - Redmine 4.x
--- init.rb
+++ init.rb
@@ -1,20 +1,24 @@
-Redmine::Plugin.register :redmine_login_bypass do
-  name 'Redmine Login Bypass'
-  description 'Login without passwords'
-  version '1.0.0'
-  url 'https://github.com/vzvu3k6k/redmine_login_bypass'
-end
-
-require 'redmine_login_bypass/hook'
-
-# ActionDispatch::Reloader is deprecated in Rails 5 (Redmine 4).
-reloader =
-  if defined? ActiveSupport::Reloader
-    ActiveSupport::Reloader
-  else
-    ActionDispatch::Reloader
+# Disable on non-development environments by default
+# so that it won't affect other plugin's tests or open a backdoor in production
+if Rails.env.development? || ENV['ENABLE_REDMINE_LOGIN_BYPASS'] == '1'
+  Redmine::Plugin.register :redmine_login_bypass do
+    name 'Redmine Login Bypass'
+    description 'Login without passwords'
+    version '1.0.0'
+    url 'https://github.com/vzvu3k6k/redmine_login_bypass'
   end
 
-reloader.to_prepare do
-  User.prepend RedmineLoginBypass::UserPatch
+  require 'redmine_login_bypass/hook'
+
+  # ActionDispatch::Reloader is deprecated in Rails 5 (Redmine 4).
+  reloader =
+    if defined? ActiveSupport::Reloader
+      ActiveSupport::Reloader
+    else
+      ActionDispatch::Reloader
+    end
+
+  reloader.to_prepare do
+    User.prepend RedmineLoginBypass::UserPatch
+  end
 end
